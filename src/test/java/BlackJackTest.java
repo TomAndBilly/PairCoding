08401f6a15ed78e9eca9d12212eaed8e8bbaad90
TestingBilly <89077205+TestingBilly@users.noreply.github.com>
2021-09-14 16:20:57 +0100

    Added Test

--- src/test/java/BlackJackTest.java
+++ src/test/java/BlackJackTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.DisplayName;
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class BlackJackTest {
+    Deck deck = New Deck;
+
+    @Test
+    @DisplayName("First Test")
+    public void testGetCardValue()
+    {
+        assertEquals(getAmount, 52, "There are 52 cards in this deck");
+
+    }
 }

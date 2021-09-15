55515cca348cb54e2026d4da02aa39b7ec183eb1
MerrickT <thomashmerrick@gmail.com>
2021-09-15 09:36:04 +0100

    BlackJackTest, first success.

---
+++ src/test/java/BlackJack/BlackJackTest.java
@@ -0,0 +1,10 @@
+package BlackJack;
+
+import org.junit.jupiter.api.DisplayName;
+import org.testng.annotations.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class BlackJackTest {
+}
